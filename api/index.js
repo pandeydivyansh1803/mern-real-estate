f999210724618ee5b4c520d91914e9ea066bcfac
pandeydivyansh1803 <pandeydivyansh1803@gmail.com>
2024-02-12 18:52:56 +0530

    connect to db and dotenv module

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// configure .env
+dotenv.config();
 
 const app = express()
-
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to db');
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000,()=>{
     console.log("server is runnin on port 3000!");
