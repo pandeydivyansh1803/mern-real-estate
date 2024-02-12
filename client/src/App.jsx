922c868fb606b186b710358894497d349d042f2d
pandeydivyansh1803 <pandeydivyansh1803@gmail.com>
2024-02-12 17:30:31 +0530

    browser router and react-router-dom

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+
+
+export default function App() {
+  return (
+    <BrowserRouter >
+    <Header/>
+    <Routes>
+      <Route path={"/"}  element={<Home></Home>}></Route>
+      <Route path={"/sign-in"}  element={<SignIn></SignIn>}></Route>
+      <Route path={"/sign-up"}  element={<SignUp></SignUp>}></Route>
+      <Route path={"/about"}  element={<About />}></Route>
+      <Route path={"/profile"}  element={<Profile />}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
+
+export default function Header() {
+  const navigate = useNavigate();
+  return (
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap cursor-pointer' onClick={()=>{
+          navigate('/')
+        }}>
+          <span className='text-slate-500'>Indian</span>
+          <span className='text-slate-700'>Estate</span>
+        </h1>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'>
+          </input>
+          <FaSearch className='text-slate-600' />
+        </form>
+        <ul className='flex gap-4 '>
+          <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer' onClick={()=>{
+            navigate('/');
+          }}>Home</li>
+          <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer' onClick={()=>{
+            navigate('/about')
+          }}>About</li>
+          <li className='sm:inline text-slate-700 hover:underline cursor-pointer' onClick={()=>{
+            navigate('/sign-in')
+          }}>SignIn</li>
+        </ul>
+
+      </div>
+
+    </header>
+  )
+}
