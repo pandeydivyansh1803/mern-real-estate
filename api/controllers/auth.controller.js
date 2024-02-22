0e1cb7074a3b44ab2907e0f1683e31620d45829e
pandeydivyansh1803 <pandeydivyansh1803@gmail.com>
2024-02-22 12:01:16 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.model")
-// const errorHandler = require("../utils/error")
-
+const errorHandler = require("../utils/error")
+const jwt = require('jsonwebtoken')
 // we need to encrypt the password to store it in db
 // we will use bcryptjs package
 const bcryptjs = require('bcryptjs');
@@ -19,4 +19,25 @@ const signup =async (req,res,next)=>{
     }
 }
 
-module.exports = signup;
+const signin = async(req,res,next)=>{
+    const {email,password} = req.body;
+    try{
+        // step 1 check if user with given email is in our db or not
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404,'User not found'))
+        }
+        // step 2 check if password matches or not
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,'Wrong credentials'));
+        }
+
+        // jwt and token
+        const token = jwt.sign({id:validUser._id,email},process.env.JWT_SECRET)
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(validUser);
+    }catch(err){
+        next(err);
+    }
+}
+module.exports = {signup,signin};
