e2436abce51b3b1af29c9168ccd8f29a144d14bb
pandeydivyansh1803 <pandeydivyansh1803@gmail.com>
2024-03-27 11:07:57 +0530

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const errorHandler = require("../utils/error");
 const User = require('../models/user.model');
-
+const Listing = require('../models/listing.model')
 const bcryptjs = require('bcryptjs');
 const test = (req,res)=>{
     res.json({
@@ -46,4 +46,20 @@ const deleteUser = async (req,res,next)=>{
         next(err);
     }
 }
-module.exports = {test,updateUser , deleteUser};
+const getUserListings = async (req,res,next)=>{
+    
+    if(req.user.id === req.params.id){
+        try{
+            const listings = await Listing.find({userRef : req.params.id})
+            res.status(200).json(listings);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    else{
+        return next(errorHandler(401,'You can only view your own listings'))
+    }
+    
+}
+module.exports = {test,updateUser , deleteUser, getUserListings};
