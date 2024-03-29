0069844740d64f59de9fc4a0f6f033bae67f7d03
pandeydivyansh1803 <pandeydivyansh1803@gmail.com>
2024-03-29 07:28:28 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createListing , deleteListing, updateListing, getListing } = require('../controllers/listing.controller.js');
+const { createListing , deleteListing, updateListing, getListing, getListings } = require('../controllers/listing.controller.js');
 const { verifyToken } = require('../utils/verifyUser');
 const router = express.Router();
 
@@ -7,4 +7,5 @@ router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
 router.post('/update/:id',verifyToken,updateListing);
 router.get("/get/:id",getListing);
+router.get('/get',getListings);
 module.exports = router;
